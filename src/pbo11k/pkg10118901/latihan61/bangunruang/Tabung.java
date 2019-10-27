645c2fe2cb0da3d3c7b224acc67a465d59bcc281
Gonior <43252085+Gonior@users.noreply.github.com>
2019-10-28 05:34:47 +0700

    Dibuat : Minggu, 28 Oktober 2019

---
+++ src/pbo11k/pkg10118901/latihan61/bangunruang/Tabung.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11k.pkg10118901.latihan61.bangunruang;
+/**
+ * @author
+ * NAMA             : Dedi cahya
+ * NIM              : 10118901
+ * KELAS            : IF11K
+ * Deskripsi Program  : Program untuk menghitung bangun ruang dengan konsep abtraks
+ * 
+ */
+public class Tabung extends BangunRuang{
+
+    @Override
+    public float hitungVolume() {
+        return (PHI*getR()*getR())* getT();    }
+    
+}
